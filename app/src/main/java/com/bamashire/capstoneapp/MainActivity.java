3ab686733f84e1ca8580bd668c0143dc0a176328
Kurt Anderson <Kurtanderson234@gmail.com>
2018-02-13 10:20:37 -0600

    Implemented Sign Up Activity
    Added in the Sign up Page. Works with connection to the server

--- app/src/main/java/com/bamashire/capstoneapp/MainActivity.java
+++ app/src/main/java/com/bamashire/capstoneapp/MainActivity.java
@@ -5,9 +5,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.GetCallback;
@@ -20,15 +23,48 @@ import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void signUp(View view) {
+        EditText usernameText = (EditText) findViewById(R.id.usernameText);
+        EditText passwordText = (EditText) findViewById(R.id.passwordText);
+        if (usernameText.getText().toString() == ""|| passwordText.getText().toString() == ""){
+           showToast("A username and password are required");
+        }
+        else {
+            ParseUser user = new ParseUser();
+
+            user.setUsername(usernameText.getText().toString());
+            user.setPassword(passwordText.getText().toString());
+
+            user.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null){
+                        Log.i("Signup","Successful");
+                        showToast("Signup Successful");
+                    }
+                    else {
+                        showToast("Failed with " + e.getMessage());
+                    }
+                }
+            });
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         ParseAnalytics.trackAppOpenedInBackground(getIntent());
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 
 }
