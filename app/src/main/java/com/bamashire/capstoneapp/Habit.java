b334ef11a58a1281f33e9936e912c6b5d574c88b
Nate Hochstetler <nhhochstetler@crimson.ua.edu>
2018-02-18 18:43:59 -0600

    Created the home activity, implementing a recyclerview to store the list of habits, the sidebar fragment, and the fab button.

---
+++ app/src/main/java/com/bamashire/capstoneapp/Habit.java
@@ -0,0 +1,22 @@
+package com.bamashire.capstoneapp;
+
+/**
+ * Created by NateHochstetler on 2/18/18.
+ */
+
+public class Habit {
+
+    private String name;
+
+    public Habit(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
